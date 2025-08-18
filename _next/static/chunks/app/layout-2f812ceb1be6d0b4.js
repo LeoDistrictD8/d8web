e24b1766bbf0ed9c853130cffb9c6516d2274235
Povindu Samarasekara <povinduchanmith@gmail.com>
2025-08-19 00:13:38 +0530

    Updates

---
+++ _next/static/chunks/app/layout-2f812ceb1be6d0b4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{2984:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,4004,23))},4004:()=>{}},e=>{e.O(0,[837,441,964,358],()=>e(e.s=2984)),_N_E=e.O()}]);
